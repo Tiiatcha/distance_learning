60474107eea280b5855e4bcdba51c49c1793bd55
Craig Davison <craigadavison77@gmail.com>
2024-11-01 18:16:42 +0000

    configuration for db

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  max: 5, // pool size (number of connections)
+  idleTimeoutMillis: 30000, // close & remove clients which have been idle > 30 seconds
+  connectionTimeoutMillis: 2000, // return an error after 2 seconds if connection could not be established
+});
+
+module.exports = pool;
